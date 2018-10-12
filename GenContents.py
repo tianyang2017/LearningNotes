c851bda04ad3ed4ed4360fcb945647e9304eeb0d
罗祥 <1366971433@qq.com>
2018-10-12 17:24:15 +0800

    用python脚本生成目录结构

---
+++ GenContents.py
@@ -0,0 +1,28 @@
+import os
+
+
+# 用于生成Makedown格式的目录
+class GenContexts:
+
+    @classmethod
+    def generate(cls, folder_path, root_url, parent_path, deep_level, catalog: list):
+        files = os.listdir(folder_path)
+        for fileName in files:
+            file_path = os.path.abspath(folder_path) + os.sep + fileName
+            full_name = parent_path + "/" + fileName
+            if os.path.isdir(file_path):
+                catalog.append(
+                    " " * 10 * deep_level + " :open_file_folder:" + "[" + fileName + "]" + "(" + root_url + full_name + ")" + "\n")
+                cls.generate(file_path, root_url, full_name, deep_level + 1, catalog)
+            elif os.path.isfile(file_path) and '.md' in fileName:
+                catalog.append(" " * 10 * deep_level + " :page_with_curl:" + fileName + "\n")
+
+
+if __name__ == '__main__':
+    catalog = []
+    GenContexts.generate(os.getcwd() + os.sep + "notes",
+                         "https://github.com/heibaiying/LearningNotes/tree/master/notes", "", 0,
+                         catalog)
+    with open("catalog.md", 'w') as f:
+        for line in catalog:
+            f.write(line)
